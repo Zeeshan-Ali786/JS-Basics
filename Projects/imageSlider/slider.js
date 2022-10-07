f28d1d410a8065051c969383afff4dd7cee858bc
Zeeshan Ali <zee03044163645@gmail.com>
2022-10-07 19:05:06 +0500

    update image slider.

--- Projects/imageSlider/slider.js
+++ Projects/imageSlider/slider.js
@@ -30,7 +30,51 @@ function slideshow(num){
     }
     // show first slide in 0 index by using flag current value
     slides[num].style.display = "block"
+
+
+    
+// color dot in current slide index 
+    let dots = document.getElementsByClassName('dot');
+
+    console.log(dots)
+    // first all dots have sliver backgroung
+    for(let i of dots){
+        i.style.background = "#bbb";
+    }
+    // 
+    dots[num].style.background = "#3722d3"
+    dots[num].style.transition = "1s"
+    console.log(dots[num])
+
+
 }
 slideshow(flag)
 
+let dot = 0;
+function slideDot(x){
+    let dotslide = document.getElementsByClassName('slide');
+
+//slide image by clicking dots 
+    //first all slides invisible 
+    for(let i of dotslide){
+        i.style.display = "none";
+    }
+    // show first slide in 0 index by using dot current value
+    dotslide[x].style.display = "block"
+
+
+// color selected dot
+    let dots = document.getElementsByClassName('dot');
+    for(let i of dots){
+        i.style.background = "#bbb";
+    }
+
+
+    dots[x].style.background = "#3722d3"
+
+    console.log(dotslide[x])
+    console.log(dots[x])
+
+}
+slideDot(dot)
 
